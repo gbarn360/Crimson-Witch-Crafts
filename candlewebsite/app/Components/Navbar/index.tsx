845b885c8f2179e2b1e09902ebc4162ff9d5d137
gbarn360 <gbarn360@gmail.com>
2024-02-26 14:14:23 -0500

    worked on basic navbar

---
+++ candlewebsite/app/Components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+
+export default function navbar() {
+    return (
+        <div className="bg-red-500 flex justify-center sticky top-0">
+            <div className="p-4">
+                <h1 className=" text-2xl mb-5 text-center">Crimson Witch Crafts</h1>
+                <div className="flex justify-center gap-10">
+                    <h1>link1</h1>
+                    <h1>link2</h1>
+                    <h1>link3</h1>
+                    <h1>link1</h1>
+
+                </div>
+            </div>
+        </div>
+    )
+}
