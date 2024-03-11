03fa38007ba8cbe0bcf84ec0f205ddfeb76bbdce
unknown <gbarn360@gmail.com>
2024-03-10 20:38:29 -0400

    minor ui changes

--- candlewebsite/app/Components/ItemContent/index.tsx
+++ candlewebsite/app/Components/ItemContent/index.tsx
@@ -25,21 +25,26 @@ export default function ItemContent({ item }: { item: Item }) {
     }
 
     return (
-        <div className="my-10 mb-40 mx-auto pt-5 md:w-5/6 2xl:w-2/3 lg:flex justify-between gap-9 ">
+        <div className="my-10 mb-40 mx-auto pt-5 md:w-5/6 2xl:w-2/3 lg:flex  gap-9 ">
 
             <div className="w-5/6 m-auto lg:w-1/2 ">
-                <img src={image} className="m-auto rounded-xl md:w-3/4" />
+                <img src={image} className="m-auto rounded-xl md:w-3/4 xl:w-2/3 "  />
                 <div className="flex justify-center gap-10 md:w-3/4 m-auto mt-2">
                     <FontAwesomeIcon onClick={() => updateImage("left")} icon={faArrowLeft} className=" p-2 text-xl cursor-pointer" />
                     <FontAwesomeIcon onClick={() => updateImage("right")} icon={faArrowRight} className=" p-2 text-xl cursor-pointer" />
                 </div>
             </div>
-            <div className="w-5/6 m-auto mt-10 lg:mt-0 flex flex-col items-center lg:items-start lg:m-0 md:w-4/5 lg:w-1/2  text-center lg:text-left ">
+            <div className="w-5/6 m-auto mt-10 lg:mt-0 flex flex-col items-center  lg:items-start lg:m-0 md:w-4/5 lg:w-1/2 text-center lg:text-left ">
+                <div>
                 <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl ">{item.name}</h1>
                 <p className="w-5/6 lg:w-4/5 mt-5 text-md xl:text-xl ">{item.description}</p>
                 <h1 className="mt-2"><span className="font-bold">Materials: </span>{item.materials.map((material, index) => <h2 key={index}>{material}</h2>)}</h1>
+                </div>
+                <div className=" w-full">
                 <h1 className="font-bold text-2xl mt-5">${item.price}</h1>
                 <button className="bg-gray-900 text-gray-50 w-1/2 p-2 rounded-sm">ADD TO BAG</button>
+                </div>
+         
 
             </div>
 
