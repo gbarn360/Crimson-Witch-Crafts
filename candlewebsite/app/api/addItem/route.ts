2fc1b99acddd5aba7079620aab379e00fc2f457e
unknown <gbarn360@gmail.com>
2024-04-03 11:37:16 -0400

    added items

--- candlewebsite/app/api/addItem/route.ts
+++ candlewebsite/app/api/addItem/route.ts
@@ -1,12 +1,29 @@
-
-
-
+var fs = require('fs');
+import axios from "axios";
 export async function POST(request:Request){
 
-    const data = await request.json()
-    console.log(data);
+        const data = await request.json();
+        data.id = 11;
+
+        fs.readFile('db.json','utf8',function callback(err:string,jsonData:string){
+            if(err){
+                console.log(err);
+            }
+            else{
+                let obj = JSON.parse(jsonData);
+                console.log(obj);
+                obj.items.push(data);
+                let json = JSON.stringify(obj);
+
+                fs.writeFile('db.json',json,'utf8',function callback(err:string){
+                })
+
+            }
+
+        })
+        return Response.json("success");
+
+        
+      
     
-   
-    
-    return Response.json({status:200})
 }
